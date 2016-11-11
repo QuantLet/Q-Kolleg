4501ff8f7cf0db05baca614ad550fd5048b300ed
Ken Schröder <k.h.b.schroder@gmail.com>
2016-11-11 18:48:34 +0100

    Added remote database connections
    Also improved uniformness of formatting

--- ExampleRMySQL.R
+++ ExampleRMySQL.R
@@ -1,6 +1,5 @@
 # R code to import data from a database into R using RMySQL
 
-
 #################################################################
 ### Setup
 #################################################################
@@ -9,42 +8,77 @@ rm(list = ls())
 graphics.off()
 
 # Installing RMySQL, Loading RMySQL
-libraries = c("RMySQL")
-lapply(libraries,function(x)if(!(x %in% installed.packages())){install.packages(x)})
-lapply(libraries,require,quietly=TRUE,character.only=TRUE)
+  libraries = c("RMySQL")
+  lapply(libraries, function(x) if(
+    !(x %in% installed.packages())){install.packages(x)})
+  lapply(libraries, require, quietly = TRUE, character.only = TRUE)
 
 #################################################################
-### RMySQL
+### RMySQL (local database)
 #################################################################
 
-# connect to world database:
-# 1. download from MySQL: https://dev.mysql.com/doc/index-other.html
+# Connect to world database:
+# 1. Download from MySQL: https://dev.mysql.com/doc/index-other.html
 # 2. Load via DBMS as database called world. 
-con = dbConnect(drv=MySQL(),
-                user="root", password = "qwertz",
-                dbname = "world", host = "localhost")
+  con = dbConnect(drv = MySQL(),
+                  user = "root", password = "qwertz",
+                  dbname = "world", host = "localhost")
 
-# list with tables in connection
-dbListTables(con)
+# List with tables in connection
+  dbListTables(con)
 
-# list variables in specific table
-dbListFields(con, "city")
+# List variables in specific table
+  dbListFields(con, "city")
 
-# run a query:
-# specify query
-qry = "SELECT * FROM city"
-# send query
-rs1 = dbSendQuery(con, qry)
-# fetch query and import in R as data.frame
-data = fetch(rs1)
-class(data)
+# Run a query:
+  # Specify query
+  qry = "SELECT * FROM city"
+  # Send query
+  rs = dbSendQuery(con, qry)
+  # Fetch query and import in R as data.frame
+  data = fetch(rs)
+  class(data)
 
-# free resources associated with rs1
-dbClearResult(rs1)
+# Free the result, so that other queries can be submitted
+  dbClearResult(rs)
 
 
-# close connection to database
-dbDisconnect(con)
+# Close connection to local database
+  dbDisconnect(con)
 
-# proceed to work with data
-head(data)
+# Proceed to work with data
+  head(data)
+  
+  
+#################################################################
+### RMySQL (remote database)
+#################################################################
+  # Establish connection with a public database
+  drv <- dbDriver("MySQL") 
+  con <- dbConnect(drv, dbname = "ensembl_mart_84", user = "anonymous", 
+                        host = "martdb.ensembl.org", port = 5316) 
+
+# Checking the tables
+  tables <- dbListTables(con)
+  length(tables)
+  # tables[substr(tables, 1, 4) == "oana"]
+  # oanatinus_gene_ensembl__gene__main seems interesting
+
+# Long, explicit way of running a query
+  qry1  = "SELECT * FROM  oanatinus_gene_ensembl__gene__main"
+  qry2 = "SELECT * FROM oanatinus_gene_ensembl__go_GO__dm"
+
+  rs1  = dbSendQuery(con, qry1)
+  data = fetch(rs1)
+  dbClearResult(rs1)
+
+  rs2   = dbSendQuery(con, qry2)
+  data2 = fetch(rs2)
+  dbClearResult(rs2)
+
+# Submit, fetch and close with one command
+  data1 = dbGetQuery(con, qry1)
+  data2 = dbGetQuery(con, qry2)
+
+# Close connections
+  dbDisconnect(con)
