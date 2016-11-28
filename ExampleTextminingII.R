664806bd2009f102c3b58573a136f897c98b855b
Ken Schröder <Ken@client201-62.wlan.hu-berlin.de>
2016-11-28 16:55:14 +0100

    Stemming and cleaning improvement

--- ExampleTextminingII.R
+++ ExampleTextminingII.R
@@ -1,70 +1,77 @@
 rm(list = ls())
 graphics.off()
 
-
-# cldr-package is not available on CRAN:
-if(!("cldr" %in% install.packages())){
-  devtools::install_version("cldr", version = "1.1.0")
-}
-
 setwd("C:/Users/Johannes/Dropbox/Digital_Economics")
-# Load required packages:
-libraries = c("topicmodels","devtools","readr", "cldr","psych","irlba","tm", "RMySQL", "SnowballC","wordcloud","slam","reshape2" ,"ggplot2","httr","magrittr","rvest","stringr","XML")
-lapply(libraries,function(x)if(!(x %in% installed.packages())){install.packages(x)})
-lapply(libraries, require, quietly = TRUE, character.only = TRUE)
 
+## Load required packages:
+  libraries = c("topicmodels", "cluster", "devtools", "readr", "cldr", "psych",
+                "irlba", "tm", "RMySQL", "SnowballC", "slam", "reshape2", 
+                "ggplot2", "httr", "magrittr", "rvest", "stringr", "XML", "stringi")
+  lapply(libraries, function(x){
+    if(!(x %in% installed.packages())){install.packages(x)}})
+  lapply(libraries, require, quietly = TRUE, character.only = TRUE)
+  
+  # cldr-package is not available on CRAN:
+  if(!("cldr" %in% installed.packages())){
+    devtools::install_version("cldr", version = "1.1.0")
+  }
+  
 #####################################################################
 ####    Text-mining from local source 
 #####################################################################
 ## Get data:
-drv = dbDriver("MySQL") 
-con = dbConnect(drv, dbname = "Q-Kolleg", 
-                user = "stoiberj2.hub", password = "",
-                host = "neyman.wiwi.hu-berlin.de", port = 3306)
-# extract the table abstracts
-allabstracts = dbGetQuery(con, "SELECT * FROM abstracts")
-# close db connection
-dbDisconnect(con)
+  drv = dbDriver("MySQL") 
+  con = dbConnect(drv, dbname = "Q-Kolleg", 
+                  user = "stoiberj2.hub", password = "",
+                  host = "neyman.wiwi.hu-berlin.de", port = 3306)
+
+  # extract the table abstracts
+    allabstracts = dbGetQuery(con, "SELECT * FROM abstracts")
+  
+  dbDisconnect(con)
 
 ## Detect and split languages: (cldr-package)
-detlang = detectLanguage(allabstracts$abstracts)
-table(detlang$detectedLanguage)
-## want only to proceed with english abstracts, 
-en.abstracts = split(allabstracts, detlang$detectedLanguage)$ENGLISH
+  detlang = detectLanguage(allabstracts$abstracts)
+  
+  # want only to proceed with english abstracts, 
+    en.abstracts = split(allabstracts, detlang$detectedLanguage)$ENGLISH
 
 ## Data preparation: (tm-package) 
-# Turn the abstract text into a "Corpus"
-corpus = Corpus(VectorSource(en.abstracts$abstracts))
+  # Turn the abstract text into a "Corpus"
+    corpus = Corpus(VectorSource(en.abstracts$abstracts))
 
-# Remove punctuation, numbers, stopwords, special characters, white space:
-corpus = tm_map(corpus, tolower)
-corpus = tm_map(corpus, removePunctuation)
-corpus = tm_map(corpus, removeNumbers)
-corpus = tm_map(corpus, removeWords, stopwords(kind="en"))
-# select those which should not be regarded as special characters: a-z
-replaceSpecialChars = function(d) {gsub("[^a-z]", " ", d)} 
-corpus = tm_map(corpus, replaceSpecialChars)
-corpus = tm_map(corpus, stripWhitespace)
+  # Remove punctuation, numbers, stopwords, special characters, white space:
+    cleanit <- function(corpus, lang = "en"){
+      corpus = tm_map(corpus, removePunctuation)
+      corpus = tm_map(corpus, removeNumbers)
+      corpus = tm_map(corpus, removeWords, stopwords(kind = paste(lang)))
+      corpus = tm_map(corpus, content_transformer(function(d){
+        gsub("[^a-z]", " ", d)}))
+      corpus = tm_map(corpus, content_transformer(function(d){
+        stri_trans_general(d, "latin-ascii")}))
+      corpus = tm_map(corpus, stripWhitespace)
+      return(corpus)
+      }
+    corpus = cleanit(corpus)
+  
+  # Stemming:
+    stemit <- function(x){
+      stemDocument(PlainTextDocument(x))}
+    corpus = tm_map(corpus, stemit)
+    
+  # Assign paper number as name:
+    names(corpus) = en.abstracts$number
 
-corpus = tm_map(corpus, PlainTextDocument)
 
-# after preprocessing, the filenames had been deleted, assign them discussion papaer number
-names(corpus) = en.abstracts$number
+## Term document matrix:
+  tdm = TermDocumentMatrix(corpus,control = list(minWordLength = 3))
+  dtm = DocumentTermMatrix(corpus, control= list(minWordLength = 3))
 
-writeLines(as.character(corpus[[1]]))
-
-# get term document matrix
-meta(corpus[[1]])
-tdm = TermDocumentMatrix(corpus,control = list(minWordLength = 3))
-dtm = DocumentTermMatrix(corpus, control= list(minWordLength = 3))
-
-inspect(dtm)
-inspect(tdm)
-# remove very sparse terms
-tdm.cmn = removeSparseTerms(tdm,0.9)
-dim(tdm); dim(tdm.cmn)
-tdm.cmn
-removeSparseTerms(tdm,0.6)$dimnames$Terms
+  # Remove very sparse terms
+    tdm.cmn = removeSparseTerms(tdm,0.9)
+    dim(tdm); dim(tdm.cmn)
+    tdm.cmn
+    removeSparseTerms(tdm,0.6)$dimnames$Terms
 
 #################################################
 # conduct here#
@@ -106,8 +113,7 @@ plot(groups, hang=-1)
 rect.hclust(groups,5)
 hier.clust = cutree(groups, 5)
 clustm = table(hier.clust, p.code)
-str(p.code)
-image(t(clustm[nrow(clustm):1,] ), axes=FALSE, zlim=c(-4,4), col=c(0,0,0,4))
+image(t(clustm[nrow(clustm):1,] ), axes=FALSE, zlim=c(-4,4), col = rainbow(10))
 plot(pca$x[,1:2], col=cutree(groups, 38), main="38 Cluster")
 plot(pca$x[,1:2], col=cutree(groups, 5), main="5 Cluster")
 
@@ -115,17 +121,14 @@ plot(pca$x[,1:2], col=cutree(groups, 5), main="5 Cluster")
 
 ### k-means
 #k means algorithm, 2 clusters, 100 starting configurations
-kfit <- kmeans(d, 38, nstart=100)
+kfit <- kmeans(d, length(unique(p.code)), nstart=100)
+kfit2 <- kmeans(d, length(unique(p2.code)), nstart = 100)
+
 #plot - need library cluster
-clusplot(m, kfit$cluster, color=T, shade=T, labels=2, lines=0)
+clusplot(m, kfit$cluster, color=T, shade=T, lines=0)
+clusplot(m, kfit2$cluster, color = T, shade = T, lines = 0)
+table(kfit2$cluster, p2.code)
 table(kfit$cluster, p.code)
 
-plot(pca$x[,1:2], col= kmeans(d, 38, nstart=100)$cluster, main="38 cluster")
-plot(pca$x[,1:2], col= kmeans(d, 5, nstart=100)$cluster, main="5 cluster")
-
-
-
-
-
-
-
+plot(pca$x[,1:2], col= kfit$cluster, main="38 cluster")
+plot(pca$x[,1:2], col= kfit2$cluster, main="5 cluster")
