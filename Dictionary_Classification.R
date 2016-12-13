47cd9703a29623c5ac48e98779d181ebcf055585
Ken Schröder <Ken@client206-163.wlan.hu-berlin.de>
2016-12-13 14:26:50 +0100

    Classifies abstracts with dictionaries
    Uses the lemmatized dictionaries and returns for every document the
    JEL-code(s) for which the correspondence between the words and
    dictionary is the highest. Note: Still need to adapt dictionaries &
    correct for length

---
+++ Dictionary_Classification.R
@@ -0,0 +1,29 @@
+library(RMySQL)
+source("helperfunctions_Q-Kolleg.R")
+
+
+####  START USING THE DICTIONARIES:
+  ## Get the data from the database:
+    # Connect to the database:
+    drv = MySQL()
+    con = dbConnect(drv, dbname = "Q-Kolleg", 
+                user = "schroedk.hub", password = "O9rVnS%J",
+                host = "neyman.wiwi.hu-berlin.de", port = 3306)
+    # Fetch the data:
+    dictionaries = dbGetQuery(con, "SELECT * FROM dictionary")
+    dbtt = dbGetQuery(con, "SELECT * FROM treetagger")
+    texts = transform(dbtt, splitwords = I(strsplit(as.character(lemma), " ")))
+
+  ## Classify all documents 
+  dict_classified = alldocs_alldicts(texts$splitwords, 
+                                      dictionaries$dict, 
+                                      dictionaries$JELcode)
+
+  ## Summarize the results and look for sensible results:
+  class_summary = summarize(dict_classified)
+
+  ## Get the letter of the most common JEL code:
+  JELraw = sapply(dbtt$jel, getletters)
+
+  ## Match between real and classified JEL:
+  performance(JELraw, dict_classified)
