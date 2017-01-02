919d3a661f7d2740de2204cc717219b68ede23ce
Ken Schröder <Ken@client247-34.wlan.hu-berlin.de>
2017-01-02 18:24:47 +0100

    Included extra classification options

--- Dictionary_Classification.R
+++ Dictionary_Classification.R
@@ -7,23 +7,41 @@ source("helperfunctions_Q-Kolleg.R")
     # Connect to the database:
     drv = MySQL()
     con = dbConnect(drv, dbname = "Q-Kolleg", 
-                user = "schroedk.hub", password = "..",
+                user = "schroedk.hub", password = "O9rVnS%J",
                 host = "neyman.wiwi.hu-berlin.de", port = 3306)
     # Fetch the data:
     dictionaries = dbGetQuery(con, "SELECT * FROM dictionary")
     dbtt = dbGetQuery(con, "SELECT * FROM treetagger")
     texts = transform(dbtt, splitwords = I(strsplit(as.character(lemma), " ")))
 
-  ## Classify all documents 
-  dict_classified = alldocs_alldicts(texts$splitwords, 
-                                      dictionaries$dict, 
-                                      dictionaries$JELcode)
-
-  ## Summarize the results and look for sensible results:
-  class_summary = summarize(dict_classified)
-
   ## Get the letter of the most common JEL code:
   JELraw = sapply(dbtt$jel, getletters)
+  
+  ## Classify all documents with duplicate words in dictionary:
+  dict_classified = alldocs_alldicts(texts$splitwords, 
+                                      dictionaries$dict, 
+                                      dictionaries$JELcode,
+                                      correct = TRUE,
+                                      binary = FALSE)
 
-  ## Match between real and classified JEL:
-  performance(JELraw, dict_classified)
+  ## Classify all documents without duplicate words in dictionary:
+  dict_unique = sapply(lapply(strsplit(dictionaries$dict, " "), unique), 
+                       paste, character(1L), collapse = "")
+  classified_unique_dict = alldocs_alldicts(texts$splitwords, 
+                                            dict_unique,
+                                            dictionaries$JELcode,
+                                            correct = TRUE,
+                                            binary = TRUE)
+  
+  ## Classify all documents without correcting for the length of the dictionary, 
+    ## and allowing multiple matches for a single word:
+  classified_unique_raw = alldocs_alldicts(texts$splitwords,
+                                           dictionaries$dict,
+                                           dictionaries$JELcode,
+                                           correct = FALSE,
+                                           binary = FALSE)
+  
+  ## Plot the performance of the classifiers:
+  performance_plot(JELraw, dict_classified, classified_unique_dict, classified_unique_raw)
+
+
